f5985dddfcca0dc03b760f2fa126626845dc3b97
henrique-marcarini <henrique_dutra_10@hotmail.com>
2023-04-04 16:32:50 -0300

    secao 02

---
+++ front-end/secao02-introducao-ao-react/dia01-hello-world-no-react/my-app/src/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class About extends React.Component {
+    render() {
+        return (
+            <div>
+            <h1>Henrique Dutra Marcarini</h1>
+                <p>Sou Henrique, estudante da Trybe</p>
+            <h2>Minhas habilidades</h2>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Javascript</li>
+
+                </ul>
+                </div>
+        )
+    }
+}
+
+export default About;
